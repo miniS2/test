f7de56b55f3e36e8c09f194b94d161f1f5682ed7
samsung <samsung@192.168.219.100>
2021-03-23 23:28:44 +0900

    dojeon

--- test/src/test/test01.java
+++ test/src/test/test01.java
@@ -2,10 +2,6 @@ package test;
 
 public class test01 {
 
-	public test01() {
-		// TODO Auto-generated constructor stub
-	}
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
